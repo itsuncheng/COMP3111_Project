d706157f5d4a6a3f2fdee96468a92a5ad3390fa1
itsuncheng <itsuncheng2000@gmail.com>
2019-12-02 18:32:55 +0800

    added javadocs for Main class

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -6,8 +6,17 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * Main driver class to start up the GUI application
+ * @author raymondcheng
+ *
+ */
 public class Main extends Application {
-
+	
+	/**
+	 * sets up the configuration for the GUI application using javafx
+	 * @param primaryStage built-in argument required by javafx to load GUI application
+	 */
     @Override
     public void start(Stage primaryStage) throws Exception{
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/sample.fxml"));
@@ -19,7 +28,10 @@ public class Main extends Application {
         appController.createArena();
     }
 
-
+    /**
+     * main function (entry point of the program) which calls the start function above to load the GUI
+     * @param args
+     */
     public static void main(String[] args) {
     	
         launch(args);
